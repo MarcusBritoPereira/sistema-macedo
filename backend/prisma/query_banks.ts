e4ffedb403fd4c3bc7fa0eb6f0086017159b6d35
Marcus Rodrigo Brito Pereira <marcusrodrigo2@gmail.com>
2026-05-29 09:44:40 -0300

    design: refactor reconciliation form to premium two-column input layout

--- backend/prisma/query_banks.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const accounts = await prisma.contaBancaria.findMany();
-  console.log('--- BANK ACCOUNTS IN DATABASE ---');
-  console.log(JSON.stringify(accounts, null, 2));
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
